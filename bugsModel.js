b15629d210190d07a144d0de0e75dc047db06605
cse-mkamble <cse.mkamble@gmail.com>
2021-07-18 13:14:57 +0530

    make it better

---
+++ bugsModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const bugsSchema = new mongoose.Schema({
+    owner: {
+        type: String,
+    },
+    title: {
+        type: String,
+    },
+    status: {
+        type: String,
+    },
+    priority: {
+        type: String,
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('bugs', bugsSchema)
--- server.js
+++ server.js
@@ -3,57 +3,57 @@ var path = require("path");
 var bodyparser = require("body-parser");
 var MongoClient = require("mongodb").MongoClient;
 var ObjectId = require('mongodb').ObjectID;
+const Bugs = require('./bugsModel')
+const mongoose = require('mongoose')
 
 var app = express();
 var db;
 
 app.set("port", process.env.PORT || 3000);
 
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/api/bugs/",(request,response) => {
-
+app.get("/api/bugs/", (request, response) => {
     var filter = {};
-    if(request.query.priority){
+    if (request.query.priority) {
         filter.priority = request.query.priority;
     }
-    if(request.query.status){
+    if (request.query.status) {
         filter.status = request.query.status;
     }
-    db.collection("bugs").find(filter).toArray((err,docs) => {
-        console.log(docs);
-        response.json(docs);
-    });
+    Bugs.find(filter).exec((err, result) => {
+        if (err) return response.status(400).json({ err })
+        if (result) return response.json(result);
+    })
 });
 
 app.use(bodyparser.json());
-app.post("/api/bugs",(request,response) => {
-    console.log(request.body)
-    var newBug = request.body;
-
-    db.collection("bugs").insertOne(newBug,(err,result) => {
-        var newId = result.insertedId;
-        console.log(newId);
-        db.collection("bugs").find({_id:newId}).next((err,docs) => {
-            response.json(docs);
-        })
-    });
+app.post("/api/bugs", async (request, response) => {
+    const { owner, title, status, priority } = request.body
+    const newBugs = new Bugs({
+        owner, title, status, priority
+    })
+    await newBugs.save()
+    response.json({ msg: "New Bugs Add!" })
 });
 
-app.put('/api/bugs/:id', function(req, res) {
-  var bug = req.body;
-  console.log("Modifying bug:", req.params.id, bug);
-  var oid = ObjectId(req.params.id);
-  db.collection("bugs").updateOne({_id: oid}, bug, function(err, result) {
-    db.collection("bugs").find({_id: oid}).next(function(err, doc) {
-      res.send(doc);
-    });
-  });
+app.put('/api/bugs/:id', async function (req, res) {
+    var bug = req.body
+    var _id = req.params.id
+    const updatedBugs = await Bugs.findOneAndUpdate({ _id }, bug, {new: true});
+    return res.status(201).json({ updatedBugs });
 });
 
-MongoClient.connect("mongodb://localhost/bugsdb",(err,dbConnection) => {
-    db = dbConnection;
-    app.listen(app.get("port"),() => {
-        console.log("I am listening at " + app.get("port"));
-    });
+mongoose.connect("mongodb+srv://root:root@cluster0.a0xhf.mongodb.net/bugtracker?retryWrites=true&w=majority", {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, err => {
+    if (err) throw err;
+    console.log("Connected to mongodb")
+})
+
+app.listen(app.get("port"), () => {
+    console.log("I am listening at " + app.get("port"));
 });
